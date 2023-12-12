1ce6bfaef4c7744904424f34dbe0704a57c1c2a9
Linh Pham <44250072+lnkphm@users.noreply.github.com>
2023-12-12 19:45:43 +0700

    imp: Add data (#7)

--- backend/main.go
+++ backend/main.go
@@ -25,6 +25,7 @@ var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ID: "4", Title: "Test", Artist: "Who", Price: 39.99},
 }
 
 func getAlbums(c *gin.Context) {
